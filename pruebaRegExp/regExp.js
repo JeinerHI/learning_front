b0442796cfcf0e536b5ac65071bd66730842fba6
JeinerHI <jeinerhur@gmail.com>
2026-01-27 14:32:35 +0100

    ultimas actualizaciones

--- pruebaRegExp/regExp.js
+++ pruebaRegExp/regExp.js
@@ -10,7 +10,7 @@ const Expresiones = {
 const form = document.getElementById("formu");
 const btnSubmit = form.querySelector("#submit");
 
-const validadorInput = (e) => {
+form.addEventListener("input", (e) => {
     if (!(e.target.value === "")) {
         if ((Expresiones[e.target.id].test(e.target.value))) {
             e.target.classList.remove("noValidado");
@@ -27,13 +27,46 @@ const validadorInput = (e) => {
         e.target.closest(".campo").querySelector(".err_message").textContent = "";
         return;
     }
-}
-
-form.addEventListener("input", (e) => {
-    validadorInput(e);
-    }
-);
+});
 
 btnSubmit.addEventListener("submit", (e) => {
     
 });
+
+/* Ejercicios REPLACE  */
+/* Dado el siguiente texto:
+let texto = "Juan Pérez Gómez";
+Usar la siguiente expresión regular:
+/\s+/g
+Objetivo:
+• eliminar los espacios duplicados
+• dejar un solo espacio entre palabras 
+
+let text = "Juan  Pérez  Gómez";
+console.log(text.replace(/\s+/g," "));*/
+
+
+
+/* Ejercicio 2 – Eliminar caracteres no numéricos
+Dado el siguiente texto:
+let telefono = "+34 612-45 78 90";
+Usar la siguiente expresión regular:
+/\D/g
+Objetivo:
+• dejar únicamente los números 
+
+let text = "+34 612-45 78 90";
+console.log(text.replace(/\D/g,""));*/
+
+
+
+/* Ejercicio 3 – Eliminar caracteres especiales
+Dado el siguiente texto:
+let usuario = "juan_perez!!2024";
+Usar la siguiente expresión regular:
+/[^\w]/g
+Objetivo:
+• eliminar caracteres especiales
+• permitir solo letras y números */
+let text = "juan_perez!!2024";
+console.log(text.replace(/[^\w]/g,""));
